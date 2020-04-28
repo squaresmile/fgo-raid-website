92ce95f7d172669c30a7ea69bf119bb686a85d8c
squaresmile <squaresmile@protonmail.com>
2020-04-28 11:08:16 -0400

    Typescript initial files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 var path = require("path");
 
 module.exports = {
-  //...
+  entry: "./src/index.ts",
+  output: {
+    filename: "./main.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
