834167d1016cd8e0109e4aca15f6fbd950da5a98
squaresmile <squaresmile@protonmail.com>
2020-05-13 16:36:01 -0400

    Remove TS
    Simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,10 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "./main.js",
-  },
   performance: {
     hints: false,
   },
   devtool: "source-map",
-  resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-      { test: /\.js$/, loader: "source-map-loader" },
-    ],
-  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
