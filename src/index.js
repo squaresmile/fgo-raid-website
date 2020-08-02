4adf61e3bd4c1165cd7cbe95a8e2653a324558f3
squaresmile <squaresmile@protonmail.com>
2020-08-02 14:48:10 -0400

    Use Highcharts for the charts (#5)
    * Highcharts initial files
    
    * Use JS instead of TS
    
    * Try synced crosshairs
    
    * Synced crosshairs aren't really slow?
    
    * Add touch events to synced crosshair
    
    * Remove TS
    
    Simplify webpack config
    
    * Update date-fns
    
    * Update highcharts and source-map-loader
    
    * Update package-lock
    
    * Updated package and added lodash
    
    * Updated the site for the deadheat race rerun
    
    * Add left margin to line up the left margins of the two charts. Synced crosshairs should be the same now.
    
    * Removed markers on charts
    
    * Made the title and lengend bigger
    
    * Longer ETA calculation time
    
    Use Boost for the charts
    
    * Updated for  the event
    
    * Added target and scale to the data
    
    * Simplified ETA calc a bit
    
    * Increased right margin for the 1000K distance
    * Better filter for the ETA time list
    
    * Updated date-fns
    
    * Stopped using depreciated Highcharts.each
    
    * Set the min yaxis of the hp chart to 0
    
    * Updated the chart for when one or two races already finished
    
    * Updated packages
    
    * Updated chart for part 2
    
    * Updated packages

---
+++ src/index.js
@@ -0,0 +1,366 @@
+import { formatDistanceToNowStrict, formatDistanceToNow } from "date-fns";
+import { format } from "date-fns-tz";
+import * as Highcharts from "highcharts";
+import Accessibility from "highcharts/modules/accessibility";
+import Exporting from "highcharts/modules/exporting";
+import ExportData from "highcharts/modules/export-data";
+import { nth } from "lodash-es";
+import Boost from "highcharts/modules/boost";
+
+const BOSS_COLOR = {
+  Fran: "#55A868",
+  Helena: "#64B5CD",
+  Nito: "#4C72B0",
+  Nobu: "#C44E52",
+  Raikou: "#8172B2",
+  Sabers: "#CCB974",
+};
+
+function createPText(string) {
+  let newPElement = document.createElement("p");
+  newPElement.textContent = string;
+  return newPElement;
+}
+
+function createHighChartsArray(dataArray, timeArray) {
+  let outArray = [];
+  for (let i = 0; i < dataArray.length; i++) {
+    outArray.push([timeArray[i] * 1000, dataArray[i]]);
+  }
+  return outArray;
+}
+
+function diffArray(origArray) {
+  let startArray = origArray.slice(0, -1);
+  let endArray = origArray.slice(1);
+  let outArray = [];
+  for (let i = 0; i < startArray.length; i++) {
+    outArray.push(endArray[i] - startArray[i]);
+  }
+  return outArray;
+}
+
+function rate(dataArray, timeArray, scale) {
+  let dataDiff = diffArray(dataArray);
+  dataDiff = dataDiff.map((x) => x * scale);
+  let timeDiff = diffArray(timeArray);
+  let outArray = [];
+  for (let i = 0; i < dataDiff.length; i++) {
+    if (dataDiff[i] > 0) {
+      outArray.push(dataDiff[i] / timeDiff[i]);
+    }
+  }
+  return outArray;
+}
+
+function calcETA(dataArray, timeArray, target, n) {
+  n = Math.min(n, dataArray.length);
+  let lastData = nth(dataArray, -1);
+  let lastTime = nth(timeArray, -1);
+  let dataDiff = lastData - nth(dataArray, -n);
+  let timeDiff = lastTime - nth(timeArray, -n);
+  let avgRate = dataDiff / timeDiff;
+  if (avgRate === 0) {
+    avgRate = Math.sign(dataDiff);
+  }
+  let timeRemaining = (target - lastData) / avgRate;
+  return lastTime + timeRemaining;
+}
+
+function futureStrings(timestamp, strict) {
+  let futureDate = new Date(timestamp * 1000);
+  let futureString = format(futureDate, "M/d HH:mm zzz");
+  let difference = "";
+  if (strict) {
+    difference = formatDistanceToNowStrict(futureDate, {
+      addSuffix: true,
+      roundingMethod: "floor",
+    });
+  } else {
+    difference = formatDistanceToNow(futureDate, {
+      addSuffix: true,
+    });
+  }
+  return [futureString, difference];
+}
+
+// function calcAPGain(timestamp) {
+//   let APGain = (timestamp * 1000 - Date.now()) / 1000 / 60 / 5;
+//   let boundedAPGain = Math.max(Math.floor(APGain), 0);
+//   let APText = `${boundedAPGain} AP will regen until the next raids.`;
+//   return createPText(APText);
+// }
+
+// function bossesListString(bosses, prefix = "") {
+//   let bossesString = prefix;
+//   if (bosses.length > 1) {
+//     bossesString += bosses.slice(0, -1).join(", ") + " and ";
+//   }
+//   bossesString += bosses.slice(-1);
+//   return bossesString;
+// }
+
+function genOpts(data, config) {
+  let series = [];
+  for (const boss in data) {
+    series.push({
+      type: "line",
+      name: boss,
+      data: data[boss],
+      color: BOSS_COLOR[boss],
+    });
+  }
+  return {
+    chart: {
+      style: {
+        fontFamily: "Fira Sans",
+      },
+      zoomType: "x",
+      marginLeft: 80,
+    },
+    title: {
+      text: config.title,
+      style: {
+        fontSize: "1.75em",
+      },
+    },
+    xAxis: {
+      title: {
+        text: "Pacific Time",
+        style: {
+          fontSize: "1.25em",
+        },
+      },
+      type: "datetime",
+      gridLineWidth: 1,
+      crosshair: {
+        // width: 2,
+        // color: "gray",
+        dashStyle: "Dash",
+      },
+      labels: {
+        style: {
+          fontSize: "1.15em",
+        },
+      },
+      events: {
+        setExtremes: config.syncExtremes,
+      },
+    },
+    yAxis: {
+      title: {
+        text: config.yAxisTitle,
+        style: {
+          fontSize: "1.25em",
+        },
+      },
+      labels: {
+        style: {
+          fontSize: "1.15em",
+        },
+      },
+      min: config.yAxisMin,
+      max: config.yAxisMax,
+    },
+    tooltip: {
+      shadow: false,
+      animation: false,
+      shared: true,
+      valueDecimals: config.valueDecimals,
+      style: {
+        fontSize: "1.15em",
+      },
+      positioner: function () {
+        return { x: 100, y: 50 };
+      },
+    },
+    legend: {
+      enabled: true,
+      itemStyle: {
+        fontSize: "1.15em",
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    exporting: {
+      sourceWidth: 1000,
+      sourceHeight: 500,
+    },
+    plotOptions: {
+      line: {
+        marker: {
+          enabled: false,
+        },
+        lineWidth: 1,
+        states: {
+          hover: false,
+        },
+        threshold: null,
+        boostThreshold: 1,
+      },
+    },
+    series: series,
+  };
+}
+
+async function main() {
+  const data = await fetch("data.json").then((response) => response.json());
+  let raidData = data["data"];
+  let targetData = data["target"];
+  let timeArray = raidData["timestamp"];
+  delete raidData["timestamp"];
+
+  let etaResult = [];
+  let bossNames = Object.keys(raidData);
+  for (let i = 0; i < bossNames.length; i++) {
+    let boss = bossNames[i];
+    if (nth(raidData[boss], -1) <= targetData[boss]) {
+      let eta = calcETA(raidData[boss], timeArray, targetData[boss], 100);
+      etaResult.push([boss, eta, i]);
+    }
+  }
+
+  const etaTextDiv = document.getElementById("etaText");
+  etaResult = etaResult.sort((a, b) => a[1] - b[1]);
+  if (etaResult.length > 3) {
+    for (const [boss, eta] of etaResult) {
+      let [dateString, difference] = futureStrings(eta, true);
+      let etaText = `${boss}: ${difference} (${dateString})`;
+      etaTextDiv.appendChild(createPText(etaText));
+    }
+  }
+
+  Accessibility(Highcharts);
+  Exporting(Highcharts);
+  ExportData(Highcharts);
+  Boost(Highcharts);
+
+  /**
+   * In order to synchronize tooltips and crosshairs, override the
+   * built-in events with handlers defined on the parent element.
+   */
+  let stillGoingBoss = Math.max(...etaResult.map((x) => x[2]));
+  ["mousemove", "mouseleave", "touchstart", "touchmove"].forEach(function (
+    eventType
+  ) {
+    document.getElementById("charts").addEventListener(eventType, function (e) {
+      for (let i = 0; i < Highcharts.charts.length; i++) {
+        let chart = Highcharts.charts[i];
+        let event = chart.pointer.normalize(e); // Find coordinates within the chart
+        let point;
+        for (let j = stillGoingBoss; j < chart.series.length && !point; j++) {
+          point = chart.series[j].searchPoint(event, true);
+        }
+        if (point) {
+          if (["mousemove", "touchmove", "touchstart"].includes(e.type)) {
+            point.onMouseOver();
+            chart.xAxis[0].drawCrosshair(event, point);
+          } else {
+            point.onMouseOut();
+            chart.tooltip.hide(point);
+            chart.xAxis[0].hideCrosshair();
+          }
+        }
+      }
+    });
+  });
+
+  /**
+   * Override the reset function, we don't need to hide the tooltips and
+   * crosshairs.
+   */
+  Highcharts.Pointer.prototype.reset = function () {
+    return undefined;
+  };
+
+  /**
+   * Highlight a point by showing tooltip, setting hover state and draw crosshair
+   */
+  Highcharts.Point.prototype.highlight = function (event) {
+    event = this.series.chart.pointer.normalize(event);
+    this.onMouseOver(); // Show the hover marker
+    this.series.chart.tooltip.refresh([this]); // Show the tooltip
+    this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+  };
+
+  /**
+   * Synchronize zooming through the setExtremes event handler.
+   */
+  function syncExtremes(e) {
+    var thisChart = this.chart;
+    if (e.trigger !== "syncExtremes") {
+      // Prevent feedback loop
+      Highcharts.charts.forEach(function (chart) {
+        if (chart !== thisChart) {
+          if (chart.xAxis[0].setExtremes) {
+            // It is null while updating
+            chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {
+              trigger: "syncExtremes",
+            });
+          }
+        }
+      });
+    }
+  }
+
+  Highcharts.setOptions({
+    lang: {
+      numericSymbols: ["K", "M", "B", "T", "P", "E"],
+    },
+    time: {
+      timezoneOffset: 7 * 60, // Pacific time zone
+    },
+  });
+
+  let scaledHpData = {};
+  for (const boss in raidData) {
+    scaledHpData[boss] = raidData[boss].map((x) =>
+      Math.max((targetData[boss] - x) / data["scale"][boss], 0)
+    );
+  }
+
+  let hpData = {};
+  for (const boss in scaledHpData) {
+    hpData[boss] = createHighChartsArray(
+      scaledHpData[boss].slice(1),
+      timeArray.slice(1)
+    );
+  }
+
+  Highcharts.chart(
+    "hpChart",
+    genOpts(hpData, {
+      title: "NA Summer Race Rerun Distance",
+      yAxisTitle: "Distance (m)",
+      yAxisMin: etaResult.length < 6 ? 0 : null,
+      yAxisMax: 1000,
+      valueDecimals: 0,
+      syncExtremes: syncExtremes,
+    })
+  );
+
+  let scaledHpDataForDps = {};
+  for (const boss in scaledHpData) {
+    scaledHpDataForDps[boss] = scaledHpData[boss].filter((x) => x > 0);
+  }
+
+  let dpsData = {};
+  for (const boss in scaledHpDataForDps) {
+    dpsData[boss] = createHighChartsArray(
+      rate(scaledHpDataForDps[boss], timeArray, -3600),
+      timeArray.slice(1)
+    );
+  }
+
+  Highcharts.chart(
+    "dpsChart",
+    genOpts(dpsData, {
+      title: "NA Summer Race Rerun Speed",
+      yAxisTitle: "Speed (m/h)",
+      valueDecimals: 2,
+      syncExtremes: syncExtremes,
+    })
+  );
+}
+main();
